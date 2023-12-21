fb31c208c4cb3b4b367896e364a3c878a517a15e
unknown <giorgioubbriaco@protonmail.com>
2023-12-21 19:44:43 +0100

    server bluetooth funzionante

--- src/FromBluetoothToServer/src/main/java/org/water/bluetooth/BluetoothServer.java
+++ src/FromBluetoothToServer/src/main/java/org/water/bluetooth/BluetoothServer.java
@@ -9,6 +9,8 @@ import java.io.InputStream;
 
 public class BluetoothServer {
 
+    private static String deviceName = "HC05";
+
     public static void main(String[] args) {
         try {
             LocalDevice localDevice = LocalDevice.getLocalDevice();
@@ -26,21 +28,34 @@ public class BluetoothServer {
                 RemoteDevice remoteDevice = RemoteDevice.getRemoteDevice(connection);
                 String remoteDeviceName = remoteDevice.getFriendlyName(true);
 
-                if ("Mi 9T Pro".equals(remoteDeviceName)) {
-                    System.out.println("Dispositivo Mi 9T Pro connesso: " + remoteDeviceName);
+                if (deviceName.equals(remoteDeviceName) || deviceName.equals("Mi 9T Pro")) {
+                    System.out.println("Dispositivo " + deviceName + " connesso: " + remoteDeviceName);
 
                     // Ricevi dati dal dispositivo connesso
                     InputStream inputStream = connection.openInputStream();
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
+                    StringBuilder receivedData = new StringBuilder();
+                    int character;
 
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        String data = new String(buffer, 0, bytesRead);
-                        System.out.println("Dati ricevuti: " + data);
+                    while ((character = inputStream.read()) != -1) {
+                        char receivedChar = (char) character;
+
+                        // Escludi il punto e virgola dalla stampa
+                        if (receivedChar != ';') {
+                            receivedData.append(receivedChar);
+                        }
+
+                        // Stampa i dati quando viene trovato il carattere ';'
+                        if (receivedChar == ';') {
+                            String data = receivedData.toString().trim();
+                            System.out.println("Dati ricevuti: " + data);
+                            receivedData.setLength(0); // Resetta il buffer per il prossimo messaggio
+                        }
                     }
 
                     // Chiudi la connessione quando hai finito
                     connection.close();
+                    System.out.println("Connessione chiusa.");
+
                 } else {
                     System.out.println("Connessione rifiutata. Dispositivo non riconosciuto: " + remoteDeviceName);
                     connection.close();
