5259f02d49f95104a862eeca81b790b75059b6ee
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2023-12-22 23:16:43 +0100

    javadoc BluetoothServerExecutor

--- src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/executor/BluetoothServerExecutor.java
+++ src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/executor/BluetoothServerExecutor.java
@@ -2,10 +2,13 @@ package org.water.bluetooth.application.executor;
 
 import java.io.IOException;
 
+/**
+ * Bluetooth Server Executor Interface.
+ */
 public interface BluetoothServerExecutor {
 
     /**
-     *
+     * Enables the execution of the Bluetooth Server Executor.
      */
     void execute() throws IOException;
 
