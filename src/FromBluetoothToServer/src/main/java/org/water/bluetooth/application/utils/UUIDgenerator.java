a70abaeb6326313eff00c5a86c4782e0e3e95ce0
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2023-12-22 23:07:32 +0100

    javadoc UUIDgenerator

--- src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/utils/UUIDgenerator.java
+++ src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/utils/UUIDgenerator.java
@@ -1,13 +1,33 @@
 package org.water.bluetooth.application.utils;
 
+import org.water.bluetooth.application.ServerApplication;
+
 import javax.bluetooth.UUID;
 
+/**
+ * Class for generating the UUID. A Universally Unique Identifier (UUID) is a globally unique 128-bit (16-byte) number
+ * that is used to identify profiles, services, and data types in a Generic Attribute (GATT) profile.
+ * {@link javax.bluetooth.UUID}
+ */
 public class UUIDgenerator {
 
+    /**
+     * Universally Unique Identifier (UUID).
+     * {@link javax.bluetooth.UUID}
+     */
     private static UUID uuid = null;
 
+
+    /**
+     * The constructor is implemented as private since the instantiation of the {@link UUIDgenerator} class is
+     * obtained via the static {@link UUIDgenerator#generate()} method.
+     */
     private UUIDgenerator() {}
 
+
+    /**
+     * Generates a Universally Unique Identifier (UUID).
+     */
     public static synchronized void generate() {
         if(uuid == null)
             uuid = new UUID(
@@ -16,6 +36,11 @@ public class UUIDgenerator {
             );
     }
 
+
+    /**
+     * Returns the generated Universally Unique Identifier (UUID).
+     * @return Universally Unique Identifier (UUID)
+     */
     public static synchronized UUID getUUID() {
         return uuid;
     }
