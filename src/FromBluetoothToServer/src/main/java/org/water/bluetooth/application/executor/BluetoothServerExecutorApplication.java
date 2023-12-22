dc34ad8da73e357c8ee32e9721b6580b3afd9cec
unknown <giorgioubbriaco@protonmail.com>
2023-12-22 17:50:20 +0100

    connessioni multiple non funzionante

--- src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/executor/BluetoothServerExecutorApplication.java
+++ src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/executor/BluetoothServerExecutorApplication.java
@@ -1,6 +1,5 @@
 package org.water.bluetooth.application.executor;
 
-import org.water.bluetooth.application.executor.connection.Connection;
 import org.water.bluetooth.application.executor.connection.RemoteConnection;
 import org.water.bluetooth.application.utils.Logging;
 import org.water.bluetooth.application.utils.UUIDgenerator;
@@ -59,52 +58,18 @@ public class BluetoothServerExecutorApplication implements BluetoothServerExecut
 
             // If the remote device is in the list of permitted remote devices then the remote connection is
             // established, otherwise the established temporary connection is closed.
-            if( Connection.allowed(remoteDeviceName) ) {
+            if( true ) {
 
                 Logging.msg(
                         remoteDeviceName + " connected to " + localDevice.getFriendlyName()
                 );
 
-                try {
-                    InputStream inputStream;
-                    StringBuilder receivedData;
-
-                    inputStream = connection.openInputStream();
-                    receivedData = new StringBuilder();
-                    int character;
-
-                    while ( (character = inputStream.read()) != -1 ) {
-                        char receivedChar = (char) character;
-
-                        // The ; character at the end of the message is excluded.
-                        if (receivedChar != ';') {
-                            receivedData.append(receivedChar);
-                        }
-
-                        // If the current character is ; then reading of the message is finished and the text can be displayed
-                        // on the screen.
-                        String data;
-                        if ( receivedChar == ';' ) {
-                            data = receivedData.toString().trim();
-                            Logging.msg(
-                                    "temperature_received: " + data
-                            );
-                            // Reset buffer for next message
-                            receivedData.setLength(0);
-                        }
-                    }
-
-                    // At the end of the data flow, the connection is closed.
-                    connection.close();
-                    Logging.msg(
-                            "The connection between " +  localDevice + " and " +  remoteDevice + " has been closed."
-                    );
-
-                } catch (IOException e) {
-                    Logging.msg(
-                            "Error during data flow."
-                    );
-                }
+                Thread remoteConnection = new RemoteConnection(
+                        localDevice,
+                        remoteDevice,
+                        connection
+                );
+                remoteConnection.start();
 
             } else {
 
