c14b5d516b36310cbd08885fb2d667900bd86de5
unknown <giorgioubbriaco@protonmail.com>
2024-01-02 22:55:11 +0100

    aggiunto altro javadoc di intestazione

--- src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/utils/Logging.java
+++ src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/utils/Logging.java
@@ -1,19 +1,36 @@
 package org.water.bluetooth.application.utils;
 
 /**
- * Allows the generation of textual logs for the management of the Bluetooth Server.
+ * The {@code Logging} class provides a simple utility for generating textual logs related to the operation of the
+ * Bluetooth Server.
+ *
+ * <p>
+ * It contains a single method, {@link #msg(String)}, which allows log messages to be printed on the terminal.
+ * Developers can use this class to log various events and information during the execution of the Bluetooth Server.
+ * </p>
+ *
+ * <p>
+ * The class is designed to be static, ensuring that the logging functionality is easily accessible without the need to
+ * instantiate an object. Log messages are printed to the terminal using the standard output stream.
+ * </p>
+ *
+ * @see System#out
+ *
+ * @version 1.0
+ * @since 2023-12-09
+ * @author gubbriaco
+ * @author agrandinetti
+ * @author fnicoletti
  */
 public class Logging {
 
     /**
-     * This static method allows log messages relating to the operation of the Bluetooth Server to be printed on the
-     * terminal.
-     * @param text Text to be printed as log.
+     * Prints the specified text as a log message on the terminal.
+     *
+     * @param text The text to be printed as a log message.
      */
     public static void msg(String text) {
-        System.out.println(
-                text
-        );
+        System.out.println(text);
     }
 
 }
--- src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/utils/UUIDgenerator.java
+++ src/FromBluetoothToServer/src/main/java/org/water/bluetooth/application/utils/UUIDgenerator.java
@@ -1,13 +1,29 @@
 package org.water.bluetooth.application.utils;
 
-import org.water.bluetooth.application.ServerApplication;
-
 import javax.bluetooth.UUID;
 
 /**
- * Class for generating the UUID. A Universally Unique Identifier (UUID) is a globally unique 128-bit (16-byte) number
- * that is used to identify profiles, services, and data types in a Generic Attribute (GATT) profile.
- * {@link javax.bluetooth.UUID}
+ * The {@code UUIDgenerator} class provides functionality for generating and accessing Universally Unique Identifiers
+ * (UUIDs). A UUID is a globally unique 128-bit (16-byte) number used to identify profiles, services, and data types in
+ * a Generic Attribute (GATT) profile.
+ *
+ * <p>
+ * This class contains methods to generate a UUID and retrieve the generated UUID. The UUID is created with a specific
+ * value and flags, and it is used in the context of the Bluetooth Server application.
+ * </p>
+ *
+ * <p>
+ * The class is designed to be static, ensuring that the UUID generation and retrieval functionality is easily
+ * accessible without the need to instantiate an object.
+ * </p>
+ *
+ * @see javax.bluetooth.UUID
+ *
+ * @version 1.0
+ * @since 2023-12-09
+ * @author gubbriaco
+ * @author agrandinetti
+ * @author fnicoletti
  */
 public class UUIDgenerator {
 
@@ -17,32 +33,29 @@ public class UUIDgenerator {
      */
     private static UUID uuid = null;
 
-
     /**
      * The constructor is implemented as private since the instantiation of the {@link UUIDgenerator} class is
      * obtained via the static {@link UUIDgenerator#generate()} method.
      */
     private UUIDgenerator() {}
 
-
     /**
      * Generates a Universally Unique Identifier (UUID).
      */
     public static synchronized void generate() {
-        if(uuid == null)
+        if (uuid == null)
             uuid = new UUID(
                     "0000110100001000800000805F9B34FB",
                     false
             );
     }
 
-
     /**
      * Returns the generated Universally Unique Identifier (UUID).
+     *
      * @return Universally Unique Identifier (UUID)
      */
     public static synchronized UUID getUUID() {
         return uuid;
     }
-
 }
