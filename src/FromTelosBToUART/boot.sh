2fc529e232005178bff6da38006ce0f48cf73b95
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-01-02 18:59:02 +0100

    set +x aggiunti in bash files

--- src/FromTelosBToUART/boot.sh
+++ src/FromTelosBToUART/boot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set +x
 
 if [ "$#" -eq 0 ]; then
   echo "Usage: $0 <argument>"
@@ -9,14 +10,26 @@ fi
 arg1="$1"
 
 source ./configMakefile.sh "$arg1"
+echo "./configMakefile.sh arg=$arg1"
+sleep 0.1
 
 source ./configMainApp.sh "$arg1"
+echo "./configMainApp.sh arg=$arg1"
+sleep 0.1
 
 source ./configMain.sh "$arg1"
+echo "./configMain.sh arg=$arg1"
+sleep 0.1
 
 source ./configUARTdriver.sh "$arg1"
+echo "./configUARTdriver.sh arg=$arg1"
+sleep 0.1
 
 source ./configWaterAnalysis.sh "$arg1"
+echo "./configWaterAnalysis.sh arg=$arg1"
+sleep 0.1
 
 source ./configQualityParamC.sh "$arg1"
+echo "./configQualityParamC.sh arg=$arg1"
+sleep 0.1
 
